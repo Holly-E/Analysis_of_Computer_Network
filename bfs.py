297736d8f3cc6b2f995061e482552c1ec5f21f77
Holly Erickson <Erickson.Holly@icloud.com>
2018-05-09 19:24:55 -0400

    Add functions to compute largest connected component size, and compute resilience.

--- bfs.py
+++ bfs.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+These functions will allow us to determine how much an attack on a node that destroys the node and its edges will affect the largest connected components of a graph.
 """
 
 # import deque to have run time O(1) for append and pop vs O(n) traditional runtime
@@ -53,4 +51,34 @@ def largest_cc_size(ugraph):
     Takes undirected graph ugraph and returns the size (integer) of 
     the largest connected component in ugraph.
     """
+    connected = cc_visited(ugraph)
+    largest = 0
+    for component in connected:
+        if len(component) > largest:
+            largest = len(component)
+            
+    return largest
+
+#print(largest_cc_size({1:[2, 3], 2: [1, 5], 3: [1], 4: [], 5: [2]})) 
     
+def compute_resilience(ugraph, attack_order):
+    """ 
+    Takes undirected graph ugraph, and a list of nodes attack_order. 
+    For each attacked node, the given node and its edges are removed 
+    from the graph. Then computes the size of the largest connected component 
+    for the resulting graph. Returns a list whose k+1th entry is the size of 
+    the largest connected component in the graph after the removal of the 
+    first k nodes. The first entry is the size of the largest connected 
+    component in the original graph.
+    """
+    resilience = [largest_cc_size(ugraph)]
+    for node in attack_order:
+        ugraph.pop(node)
+        for _dummy_key, value in ugraph.items():
+            if node in value:
+                value.remove(node)
+        resilience.append(largest_cc_size(ugraph))
+
+    return resilience  
+
+#print(compute_resilience({1:[2, 3], 2: [1, 5], 3: [1], 4: [], 5: [2]}, [2, 5])) 
